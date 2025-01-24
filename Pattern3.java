1c16dd262b4bc8f010889b113002a02f625baf00
anvith0004 <anvithreddy64@gmail.com>
2025-01-24 14:47:43 +0530

    first commit

---
+++ Pattern3.java
@@ -0,0 +1,14 @@
+public class Pattern3 {
+public static void main(String[] args) {
+	      for(int r=1,sp=10;r<=5;r++) {
+			 for( int k=1;k<sp;k++) {
+				 System.out.println(" "); 
+			 }
+			 sp=sp-1;
+			for(int c=1;c<=r;c++){
+				System.out.print(r+"");
+			}
+			System.out.println();
+		}
+	}
+}
