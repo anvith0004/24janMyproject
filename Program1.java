ddea7cc91ec8daf8bc705b2ffa09780102fd62c9
anvith0004 <anvithreddy64@gmail.com>
2025-01-27 10:32:01 +0530

    Add files via upload

--- Program1.java
+++ Program1.java
@@ -1,4 +1,40 @@
+package MyProject;
 
 public class Program1 {
+	static void arRR(int[] x) {
+		int len =x.length;// 5
+		int arrlen =len-1;//4
+		int t=x[arrlen];
+		// num[4]=num[3]
+		//num[3]=num[2]
+		//num[2]=num[1]
+		//num[1]=num[0]
+		for (int i = arrlen; i>=1; i--) {
+			x[i]=x[i-1];
+		}
+		x[0]=t;
+		System.out.println("\n after 1 right notation \n");
+		for (int i=0;i<=arrlen;i++) {
+			System.out.print(x[i]+"  ");
+		}
+	}
+	public static void main(String[]args) {
+		
+			int[]num= {10,20,30,40,50};
+			System.out.println("Befoe right notation");
+			for (int i =0; i< num.length; i++) {
+				System.out.print(num [i]+"   ");
+			}
+			arRR(num);
+			arRR(num);
+			arRR(num);
+			
+			
+				
+			}
+		
+		
+		
+	}
 
-}
+
